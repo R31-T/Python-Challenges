6ece65aec5eb73cca296d019a5223b41d554f8f2
ReiDaNoob <48354718+ReiDaNoob@users.noreply.github.com>
2020-09-19 14:35:12 +0800

    Add files via upload

---
+++ A-Level/AS78.py
@@ -0,0 +1,34 @@
+def happy():
+    print("      _.-------._")
+    print("    .'  _     _  '.")
+    print("   |    O     O    |")
+    print("  |                 |")
+    print("  |  \           /  |")
+    print("   \  '.       .'  /")
+    print("    '.  `'---'`  .'")
+    print("      '-._____.-'")
+    
+def sad():
+    print("     .--------.")
+    print("   .'          '.")
+    print("  |   O      O   |")
+    print(" :           `    :")
+    print(" |                |")
+    print(" :    .------.    :")
+    print("  \  '        '  /")
+    print("   '.          .'")
+    print("     '-......-'")
+    
+def surprised():
+    print("      _.-'''''-._")
+    print("    .'  _     _  '.")
+    print("   |   (o)   (o)   |")
+    print("  |    _________    |")
+    print("  |   |         |   |")
+    print("   \   |       |    /")
+    print("    '.  `'---'`  .'")
+    print("      '-._____.-'")
+    
+happy()
+sad()
+surprised()
