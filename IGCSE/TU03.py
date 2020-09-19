fd47155f20d4b2c0b94e090b3cba8857756d1cff
ReiDaNoob <48354718+ReiDaNoob@users.noreply.github.com>
2020-09-19 14:25:02 +0800

    Add files via upload

---
+++ IGCSE/TU03.py
@@ -0,0 +1,11 @@
+#Exercise 1
+ComputerAge=3
+MyAge = int(input('What is your age?'))
+print('I am younger than %d, because I am %d' % (MyAge,ComputerAge))
+print(MyAge,'is a very nice age to be')
+
+#Exercise 2
+ComputerAge=345.345
+ComputerName='Skynet'
+MyAge = int(input('What is your age?'))
+print('I am older than %d, because I am %.3f My name is %s.' % (MyAge,ComputerAge,ComputerName))
